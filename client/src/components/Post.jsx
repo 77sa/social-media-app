8625a950a23d0be679dfd81111e336e5e36cb306
gavin <gavinjstone@mail.com>
2021-10-14 10:13:25 +0100

    comment ui

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { TextField, Button, ThemeProvider } from "@mui/material";
+import { theme } from "../mui-theme";
+
 import "./post.css";
 
 const Post = ({ post, currentUser, deletePost, likePost }) => {
@@ -12,13 +15,47 @@ const Post = ({ post, currentUser, deletePost, likePost }) => {
       <p>{post.content}</p>
       <span>{post.date.slice(0, 24)}</span>
       <hr />
-      <span>{post.likes}</span>
-      <button onClick={() => likePost(post._id)}>
-        {post.likedBy.includes(currentUser.username) ? "unlike" : "like"}
-      </button>
-      {currentUser.username === post.username && (
-        <button onClick={() => deletePost(post._id)}>Delete</button>
+      {post.likes > 0 && (
+        <>
+          <span>{post.likes}</span>
+          <hr />
+        </>
       )}
+      <div className="like-comment">
+        <button onClick={() => likePost(post._id)}>
+          {post.likedBy.includes(currentUser.username) ? "unlike" : "like"}
+        </button>
+        {currentUser.username === post.username && (
+          <button onClick={() => deletePost(post._id)}>Delete</button>
+        )}
+      </div>
+      <hr />
+      <ThemeProvider theme={theme}>
+        <div className="comment">
+          <TextField
+            size="small"
+            id="filled-basic"
+            variant="filled"
+            label="Comment"
+            style={{ background: "white", width: "80%" }}
+            InputProps={{ disableUnderline: true }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{
+              width: "20%",
+              minHeight: "48px",
+              borderRadius: "0px",
+              boxShadow: "none",
+            }}
+          >
+            Send
+          </Button>
+        </div>
+      </ThemeProvider>
     </div>
   );
 };
