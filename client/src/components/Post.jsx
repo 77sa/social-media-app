3017480c5848943ca342672c68f49bddbbb8651c
gavin <gavinjstone@mail.com>
2021-10-21 18:39:17 +0100

    edit post on front end

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { getConfig } from "../utils/reqConfig";
 import Comments from "./Comment";
 
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Backdrop } from "@mui/material";
 
 import "./post.css";
 
@@ -14,6 +14,8 @@ const Post = ({ post, getPosts, setError }) => {
     const [comment, setComment] = useState("");
     const [showComments, setShowComments] = useState(false);
     const [showDropdown, toggleShowDropdown] = useState(false);
+    const [showEdit, toggleShowEdit] = useState(false);
+    const [changes, setChanges] = useState("");
 
     const deletePost = async (id) => {
         try {
@@ -52,9 +54,29 @@ const Post = ({ post, getPosts, setError }) => {
         }
     };
 
-    const editPost = async (id) => {
-        alert("TODO");
+    const editPost = async () => {
         toggleShowDropdown(false);
+        toggleShowEdit(true);
+        setChanges(post.content);
+    };
+
+    const saveEdit = async (id) => {
+        try {
+            await axios.patch(
+                `/api/posts/edit/${id}`,
+                { content: changes },
+                getConfig()
+            );
+            toggleShowEdit(false);
+            getPosts();
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    };
+
+    const toggleEditBackdrop = () => {
+        toggleShowEdit(false);
+        setChanges(post.content);
     };
 
     const toggleDropdown = () => {
@@ -62,113 +84,153 @@ const Post = ({ post, getPosts, setError }) => {
     };
 
     return (
-        <div className="post">
-            <div className="post-header">
-                <div className="name-date">
-                    <h3>
-                        <Link to={`/profile/${post.username}`}>
-                            {post.username}
-                        </Link>
-                    </h3>
-                    <h6>{post.date.slice(4, 24)}</h6>
+        <>
+            {showEdit && (
+                <div className="edit-post">
+                    <div className="edit-post-flex">
+                        <p>Your post: {post.content}</p>
+                        <TextField
+                            className="textfield"
+                            id="filled-basic"
+                            variant="filled"
+                            style={{ background: "white" }}
+                            multiline
+                            rows={2}
+                            value={changes}
+                            onChange={(e) => setChanges(e.target.value)}
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            style={{ marginTop: "10px", width: "100%" }}
+                            size="large"
+                            onClick={() => saveEdit(post._id)}
+                            disabled={post.content === changes.trim()}
+                        >
+                            Save Changes
+                        </Button>
+                    </div>
+                </div>
+            )}
+            <div className="post">
+                <Backdrop
+                    sx={{
+                        color: "#fff",
+                        zIndex: (theme) => theme.zIndex.drawer + 1,
+                    }}
+                    open={showEdit}
+                    onClick={() => toggleEditBackdrop()}
+                />
+
+                <div className="post-header">
+                    <div className="name-date">
+                        <h3>
+                            <Link to={`/profile/${post.username}`}>
+                                {post.username}
+                            </Link>
+                        </h3>
+                        <h6>{post.date.slice(4, 24)}</h6>
+                    </div>
+
+                    {currentUser.username === post.username && (
+                        <div className="dropdown">
+                            <div
+                                className="dropdown-menu-button"
+                                onClick={() => toggleDropdown()}
+                            >
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                            {showDropdown && (
+                                <div className="dropdown-contents">
+                                    <a onClick={() => editPost()}>Edit</a>
+                                    <a onClick={() => deletePost(post._id)}>
+                                        Delete
+                                    </a>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
+                <hr />
+                <p>{post.content}</p>
+                <hr />
+                {post.likes > 0 && (
+                    <>
+                        <span>{post.likes}</span>
+                        <hr />
+                    </>
+                )}
+                <div className="like-comment">
+                    <button onClick={() => likePost(post._id)}>
+                        {post.likedBy.includes(currentUser.username)
+                            ? "unlike"
+                            : "like"}
+                    </button>
+                </div>
+                <hr />
+
+                <div className="post-comment">
+                    <TextField
+                        size="small"
+                        id="filled-basic"
+                        variant="filled"
+                        label="Comment"
+                        style={{
+                            background: "white",
+                            width: "80%",
+                        }}
+                        autoComplete="off"
+                        InputProps={{ disableUnderline: true }}
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        style={{
+                            width: "20%",
+                            minHeight: "48px",
+                            borderRadius: "0px",
+                            boxShadow: "none",
+                        }}
+                        disabled={!comment}
+                        onClick={() => commentPost(post._id)}
+                    >
+                        Post
+                    </Button>
                 </div>
 
-                {currentUser.username === post.username && (
-                    <div className="dropdown">
-                        <div
-                            className="dropdown-menu-button"
-                            onClick={() => toggleDropdown()}
-                        >
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                        {showDropdown && (
-                            <div className="dropdown-contents">
-                                <a onClick={() => editPost()}>Edit</a>
-                                <a onClick={() => deletePost(post._id)}>
-                                    Delete
-                                </a>
-                            </div>
-                        )}
-                    </div>
-                )}
+                <div className="toggle-comments">
+                    {showComments && post.comments.length > 0 && (
+                        <a onClick={() => setShowComments(false)}>
+                            Hide comments
+                        </a>
+                    )}
+                    {showComments
+                        ? post.comments.map((comment) => {
+                              return (
+                                  <div key={comment._id}>
+                                      <Comments
+                                          postid={post._id}
+                                          comment={comment}
+                                          getPosts={getPosts}
+                                          setError={setError}
+                                      />
+                                  </div>
+                              );
+                          })
+                        : post.comments.length > 0 && (
+                              <a onClick={() => setShowComments(true)}>
+                                  Show comments
+                              </a>
+                          )}
+                </div>
             </div>
-            <hr />
-            <p>{post.content}</p>
-            <hr />
-            {post.likes > 0 && (
-                <>
-                    <span>{post.likes}</span>
-                    <hr />
-                </>
-            )}
-            <div className="like-comment">
-                <button onClick={() => likePost(post._id)}>
-                    {post.likedBy.includes(currentUser.username)
-                        ? "unlike"
-                        : "like"}
-                </button>
-            </div>
-            <hr />
-
-            <div className="post-comment">
-                <TextField
-                    size="small"
-                    id="filled-basic"
-                    variant="filled"
-                    label="Comment"
-                    style={{
-                        background: "white",
-                        width: "80%",
-                    }}
-                    autoComplete="off"
-                    InputProps={{ disableUnderline: true }}
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                />
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    style={{
-                        width: "20%",
-                        minHeight: "48px",
-                        borderRadius: "0px",
-                        boxShadow: "none",
-                    }}
-                    disabled={!comment}
-                    onClick={() => commentPost(post._id)}
-                >
-                    Post
-                </Button>
-            </div>
-
-            <div className="toggle-comments">
-                {showComments && post.comments.length > 0 && (
-                    <a onClick={() => setShowComments(false)}>Hide comments</a>
-                )}
-                {showComments
-                    ? post.comments.map((comment) => {
-                          return (
-                              <div key={comment._id}>
-                                  <Comments
-                                      postid={post._id}
-                                      comment={comment}
-                                      getPosts={getPosts}
-                                      setError={setError}
-                                  />
-                              </div>
-                          );
-                      })
-                    : post.comments.length > 0 && (
-                          <a onClick={() => setShowComments(true)}>
-                              Show comments
-                          </a>
-                      )}
-            </div>
-        </div>
+        </>
     );
 };
 
